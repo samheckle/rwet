179e6eeaa28dff79f8c3cb81b4f1dba8ae10452a
samheckle <samheckle@yahoo.com>
2022-05-09 12:11:03 -0400

    added files

--- final/main.js
+++ final/main.js
@@ -1,29 +1,48 @@
-// const myHeaders = new Headers();
+let words = []
 
-// const myRequest = new Request("https://minimal-python3-flask.glitch.me/", {
-//   method: 'GET',
-//   headers: myHeaders,
-//   mode: 'no-cors',
-//   cache: 'default',
-// });
+const request = async () => {
+  const response = await fetch("https://planet-green-flute.glitch.me/", {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+  const json = await response.json()
+  console.log(json)
+  words = json.split(' ')
+  words = words.filter(n => n)
+  appendWords(words)
+}
 
-// fetch(myRequest)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
+const appendWords = (w) =>{
+  for(let i = 0; i < w.length; i++){
+    var p = document.createElement('p')
+    p.innerHTML = w[i]
+    p.style.top = i * (500/w.length) + 'px'
+    let img = document.getElementById('background')
+    let width = img.clientWidth
+    let offset = window.innerWidth/3
+    p.style.left = getRandomInt(offset, offset + width/2) + 'px'
+    document.getElementById('content').appendChild(p)
+  }
+}
 
-fetch("https://minimal-python3-flask.glitch.me/")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-// fetch(url, {
-//   method: "POST", // *GET, POST, PUT, DELETE, etc.
-//   mode: "cors", // no-cors, *cors, same-origin
-//   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//   credentials: "same-origin", // include, *same-origin, omit
-//   headers: {
-//     "Content-Type": "application/json",
-//     // 'Content-Type': 'application/x-www-form-urlencoded',
-//   },
-//   redirect: "follow", // manual, *follow, error
-//   referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//   body: JSON.stringify(data), // body data type must match "Content-Type" header
-// });
+request()
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+// setting images
+let int = getRandomInt(1,29)
+let imgUrl = 'images/' + int + '.jpg'
+console.log(imgUrl)
+// document.getElementById("content").style.backgroundImage = "url("+imgUrl+")"
+var elem = document.createElement("img");
+elem.setAttribute("src", imgUrl);
+elem.setAttribute("height", "100%");
+elem.setAttribute("alt", "Flower");
+elem.setAttribute('id', 'background')
+document.getElementById("content").appendChild(elem);
